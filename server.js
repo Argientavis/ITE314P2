ab350acae0ab495a7708ec377541c69d13c5f439
Argientavis <gatdulajames10@gmail.com>
2024-09-10 10:05:25 +0800

    Add root route to handle /

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // Use PORT from environment or default to 3000
 
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Node.js server!');
+});
 
+// Existing routes
 app.get('/subject', (req, res) => {
   res.send('ITE314');
 });
-
 
 app.get('/exam', (req, res) => {
   res.send('P2');
 });
 
-
 app.get('/node', (req, res) => {
   res.send('NODEJS');
 });
-
 
 app.get('/mydata', (req, res) => {
   res.json({
